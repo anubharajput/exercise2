5c314506094ba3cdba47beedfd817aeb384e0979
anubha <anubha.rajput@innotechteam.in>
2024-04-20 12:17:32 +0530

    complete 3 steps

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,68 @@
-<script setup>
-
+<script>
+import Model from './Components/model.vue';
+export default {
+    
+   data(){
+    return{
+        message:null,
+        hideShow:false,
+        showComponent:false
+            }
+   },
+   methods:{
+      onFocus(){
+        this.$refs.inputRef.focus();
+      },
+      customFunction(){
+        this.showComponent=!this.showComponent;
+      }
+   },
+   components: {
+    Model
+  }
+}
 </script>
 
 <template>
- <h1>Hello</h1>
+     <body>
+<div class="main-container">
+    <h1>Model Project</h1>
+<input type="text" v-model="message" placeholder="edit me" ref="inputRef"/>
+<p>Message is: {{ message }}</p>
+<button @click="onFocus">FOCUS</button>
+<button @click="customFunction">Open Model</button>
+<Model v-if="showComponent" :customFunction="customFunction"/>
+
+</div>
+</body>
 </template>
 
 <style scoped>
-
+body{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+.main-container{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+input{
+    height: 26px;
+    width: 11rem;
+    border-radius: 7px;
+    border-color:rgb(242, 234, 234);
+}
+button{
+    padding:5px 10px;
+    background-color: white;
+    margin-bottom: 10px;
+}
+button:hover{
+    background-color: black;
+    color:white;
+}
 </style>
+
---
+++ src/Components/model.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="empty-container" @click="customFunction()"></div>
+    <div class="box">
+        <h1>Sign up for the Giveaway!</h1>
+        <p>Grab your ninga swag for half price!</p>
+        <p>Do It Fast!!!</p>
+        <div class="buttons">
+            <button>Sign Up
+            </button>
+            <button>Login</button>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: ['customFunction'],
+}
+</script>
+<style scoped>
+.box {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background-color: beige;
+    padding: 20px;
+    width: 30%;
+    margin: auto;
+    margin-top: 50px;
+    border-radius: 10px;
+    z-index: 1;
+}
+
+.box h1 {
+    color: rgb(10, 212, 182);
+}
+
+.box .buttons {
+    display: flex;
+    gap: 20px;
+}
+
+button {
+    color: rgb(10, 212, 182);
+    border-color: rgb(10, 212, 182);
+    background-color: white;
+}
+
+button:hover {
+    background-color: rgb(10, 212, 182);
+    color: white;
+}
+
+.empty-container {
+    position: absolute;
+    inset: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, .7);
+    cursor: pointer;
+}
+</style>
