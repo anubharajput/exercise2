09bab4bd5355798ca8d4d5a93ded06ab230b2322
anubha <anubha.rajput@innotechteam.in>
2024-04-20 15:06:23 +0530

    complete all steps

--- src/App.vue
+++ src/App.vue
@@ -1,68 +1,115 @@
 <script>
 import Model from './Components/model.vue';
 export default {
-    
-   data(){
-    return{
-        message:null,
-        hideShow:false,
-        showComponent:false
-            }
-   },
-   methods:{
-      onFocus(){
-        this.$refs.inputRef.focus();
-      },
-      customFunction(){
-        this.showComponent=!this.showComponent;
-      }
-   },
-   components: {
-    Model
-  }
+
+    data() {
+        return {
+            message: null,
+            hideShow: false,
+            showComponent: false,
+            theme: 'sale'
+        }
+    },
+    methods: {
+        onFocus() {
+            this.$refs.inputRef.focus();
+        },
+        customFunction() {
+            this.showComponent = !this.showComponent;
+        }
+    },
+    components: {
+        Model
+    }
 }
 </script>
-
 <template>
-     <body>
-<div class="main-container">
-    <h1>Model Project</h1>
-<input type="text" v-model="message" placeholder="edit me" ref="inputRef"/>
-<p>Message is: {{ message }}</p>
-<button @click="onFocus">FOCUS</button>
-<button @click="customFunction">Open Model</button>
-<Model v-if="showComponent" :customFunction="customFunction"/>
-
-</div>
-</body>
-</template>
+    <div class="main-container">
+      <h1>Model Project</h1>
+      <input type="text" placeholder="edit me" ref="inputRef" />
+      <button @click="onFocus">FOCUS</button>
+      <button @click="customFunction">Open Model</button>
+      <Model v-if="showComponent" :customFunction="customFunction" :title="`Sign up for the Giveaway!`"
+          :content="`Grab your ninga swag for half price!`" :theme="theme">
+        <template v-slot:links >
+          <div class="container">
+            <p :class="{'text-color': theme === 'sale', 'content': theme !== 'sale'}">Do It Fast!!!</p>
+            <div class="buttons">
+              <button :class="{'theme-btn-color': theme === 'sale', 'btn-color': theme !== 'sale'}">Sign Up</button>
+              <button :class="{'theme-btn-color': theme === 'sale', 'btn-color': theme !== 'sale'}">Login</button>
+            </div>
+          </div>
+        </template>
+      </Model>
+    </div>
+  </template>
 
 <style scoped>
-body{
+body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
 }
-.main-container{
+
+.main-container {
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    font-family: sans-serif;
 }
-input{
+
+input {
     height: 26px;
     width: 11rem;
     border-radius: 7px;
-    border-color:rgb(242, 234, 234);
+    margin-bottom:20px;
+    border:none;
+    border:2px solid rgb(169, 167, 167)
 }
-button{
-    padding:5px 10px;
+
+ button {
+    padding: 5px 10px;
     background-color: white;
     margin-bottom: 10px;
+    border:none;
+    border:2px solid rgb(169, 167, 167);
+    border-radius: 5px;
 }
-button:hover{
+
+ button:hover {
     background-color: black;
+    color: white;
+}
+.container .buttons {
+    display: flex;
+    gap: 20px;
+}
+
+.container .btn-color {
+    color: rgb(10, 212, 182);
+    border-color: rgb(10, 212, 182);
+    background-color: white;
+}
+
+.container .btn-color:hover {
+    background-color: rgb(10, 212, 182);
+    color: white;
+}
+.container .theme-btn-color {
     color:white;
+    border-color:white;
+    background-color: red;
+}
+
+.container .theme-btn-color:hover {
+    background-color:white;
+    color: red;
+}
+.container .text-color{
+    color:white
+}
+.container .content{
+    color:black;
 }
 </style>
-
--- src/Components/model.vue
+++ src/Components/model.vue
@@ -1,54 +1,40 @@
 <template>
     <div class="empty-container" @click="customFunction()"></div>
-    <div class="box">
-        <h1>Sign up for the Giveaway!</h1>
-        <p>Grab your ninga swag for half price!</p>
-        <p>Do It Fast!!!</p>
-        <div class="buttons">
-            <button>Sign Up
-            </button>
-            <button>Login</button>
-        </div>
+    <div class="box" :class="{'text-bg':theme==='sale'}">
+        <h1 :class="{'text-color': theme === 'sale', 'title': theme !== 'sale'}">
+            {{ title }}
+        </h1>
+        <p :class="{'text-color': theme === 'sale'}">{{ content }}</p>
+        <div class="modal-content">
+      <slot name="links"></slot>
+    </div>
     </div>
 </template>
 <script>
 export default {
-    props: ['customFunction'],
+    props: ['customFunction','title','content','theme'],
 }
 </script>
 <style scoped>
 .box {
+    position: absolute;
     display: flex;
     flex-direction: column;
     align-items: center;
-    background-color: beige;
+    text-align: center;
+    background-color: white;
     padding: 20px;
     width: 30%;
     margin: auto;
-    margin-top: 50px;
+    margin-top: 100px;
     border-radius: 10px;
     z-index: 1;
 }
 
-.box h1 {
+.box .title {
     color: rgb(10, 212, 182);
 }
 
-.box .buttons {
-    display: flex;
-    gap: 20px;
-}
-
-button {
-    color: rgb(10, 212, 182);
-    border-color: rgb(10, 212, 182);
-    background-color: white;
-}
-
-button:hover {
-    background-color: rgb(10, 212, 182);
-    color: white;
-}
 
 .empty-container {
     position: absolute;
@@ -58,4 +44,11 @@ button:hover {
     background-color: rgba(0, 0, 0, .7);
     cursor: pointer;
 }
+.text-color{
+    color:white;
+}
+.text-bg{
+    background-color: red;
+}
+
 </style>
